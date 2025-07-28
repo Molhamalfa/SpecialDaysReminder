ebfcd41ee0d950e9ab4e489990b03d7d9327ee91
Molham Alfakhri <molham.business03@gmail.com>
2025-07-28 18:19:17 +0300

    Initial Commit

---
+++ SpecialDaysReminder/SpecialDaysReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialDaysReminderApp.swift
+//  SpecialDaysReminder
+//
+//  Created by Mac on 28.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpecialDaysReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
